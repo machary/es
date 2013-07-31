59603d86caf2ea8f71c03e8002796a0182d65db8
hari <hari.santoso@sangkuriang.co.id>
2013-07-31 15:10:22 +0700

    31 Juli 2013 Add some code to my shortcode-saya.php
    Signed-off-by: hari <hari.santoso@sangkuriang.co.id>

--- wp-content/themes/klasik/includes/shortcodes/shortcode-saya.php
+++ wp-content/themes/klasik/includes/shortcodes/shortcode-saya.php
@@ -17,6 +17,7 @@ function category_based_function($atts, $content = null) {
 
     extract(shortcode_atts(array(
         "label" => 'Label',
+        "pageid"=>'',
         "cat" => '',
         "class" => '',
         "order"	=>	'date',
@@ -24,22 +25,16 @@ function category_based_function($atts, $content = null) {
         "showposts" => 3,
         "disablemore" => "yes",
         "moretext" => __('Continue Reading', 'templatesquare'),
-        "lengthchar" => 30
+        "lengthchar" => 60
 
     ), $atts));
-
-    $cols = intval($cols);
-
-    if(!is_numeric($cols) || $cols < 1 || $cols > 6){
-        $cols = 4;
-    }
 
     $showposts = (!is_numeric($showposts))? 12 : $showposts;
     $longdesc = (!is_numeric($lengthchar))? 70 : $lengthchar;
     $disablemore = ($disablemore=="yes")? true : false;
 
     $output = "";
-    $output .='<div class="recentpost-container '.$class.'" style="float:left;margin-bottom:10px;width:350px;">';
+    $output .='<div class="recentpost-container six columns">';
     $output .='<h3 style="width:300px;">'. $label .'</h3>';
     $output .='<div style="min-height:135px;" >';
     global $wp_query;
@@ -60,34 +55,8 @@ function category_based_function($atts, $content = null) {
     global $post;
 
     if ($wp_query->have_posts()) :
-        $x = 0;
+
         while ($wp_query->have_posts()) : $wp_query->the_post();
-
-            $custom = get_post_custom($post->ID);
-
-            $x++;
-
-            if($cols==1){
-                $colclass = "one_half";
-            }elseif($cols==2){
-                $colclass = "one_half";
-            }elseif($cols==3){
-                $colclass = "one_third";
-            }elseif($cols==4){
-                $colclass = "one_fourth";
-            }elseif($cols==5){
-                $colclass = "one_fifth";
-            }elseif($cols==6){
-                $colclass = "one_sixth";
-            }
-
-            if($x%$cols==0){
-                $omega = "omega";
-            }elseif($x%$cols==1){
-                $omega = "alpha";
-            }else{
-                $omega = "";
-            }
 
             //$output .='<div class="'.$colclass.' columns '.$omega.'">';
             $output .='<div style="position:relative;">';
@@ -113,12 +82,99 @@ function category_based_function($atts, $content = null) {
     $output .='</div>';
     $output.='<div class="clear"></div>';
 
-
     //$counter = wp_count_posts('post')->published;
+    $pagelink = get_page_link($pageid);
     $counter = count_post_category($cat);
-    $output .='<a href="#" style="padding:5px 10px;background-color:#aeaeae;"> lihat '.$counter.' jawaban lainnya</a>';
+    $output .='<a href="'. $pagelink .'" style="padding:5px 10px;background-color:#aeaeae;"> lihat '.$counter.' jawaban lainnya</a>';
     $output .='</div>';
     return $output;
 }
 
 add_shortcode('category-based', 'category_based_function');
+
+function horizontal_line_function(){
+    $output ='<hr>';
+    return $output;
+}
+add_shortcode('separator-line', 'horizontal_line_function');
+
+function category_post_page_function($atts, $content = null) {
+
+    extract(shortcode_atts(array(
+        "label" => 'Daftar Jawaban',
+        "cat" => '',
+        "order"	=>	'date',
+        "cols" => '1',
+        "disablemore" => "yes",
+        "moretext" => __('Continue Reading', 'templatesquare'),
+        "lengthchar" => 70
+
+    ), $atts));
+
+    $longdesc = (!is_numeric($lengthchar))? 70 : $lengthchar;
+    $disablemore = ($disablemore=="yes")? true : false;
+
+    $output = "";
+    $output .='<div class="recentpost-container ten columns">';
+    $output .='<h3 style="width:300px;">'. $label .'</h3>';
+    $output .='<div style="min-height:135px;" >';
+    global $wp_query;
+    $original_query = $wp_query;
+    $wp_query = null;
+
+    $args = array(
+        "post_type" => "post",
+        "orderby" => $order
+    );
+
+    if($cat!=""){
+        $args['category_name'] =  $cat;
+    }
+
+    $wp_query = new WP_Query( $args );
+    global $post;
+
+    if ($wp_query->have_posts()) :
+        $x = 0;
+        while ($wp_query->have_posts()) : $wp_query->the_post();
+
+            $x++;
+
+            $output .='<div style="position:relative;">';
+            $output .='<div class="item-container">';
+            $postformat = 'category';
+
+            $output .= call_user_func('recent_content_'.$postformat, $longdesc, $moretext, $disablemore);
+
+            $output.='<div class="clear"></div>';
+            $output .='</div>';
+            $output .='</div>';
+
+        endwhile;
+
+        $wp_query = null;
+        $wp_query = $original_query;
+        wp_reset_postdata();
+
+    endif;
+    $output.='<div class="clear"></div>';
+    $output .='</div>';
+    $output.='<div class="clear"></div>';
+
+    $counter = count_post_category($cat);
+    $output .='<h3> Total '.$counter.' jawaban </h3>';
+    $output .='</div>';
+    return $output;
+}
+add_shortcode('daftar-jawaban', 'category_post_page_function');
+
+function category_posts_list_function($atts){
+    extract(shortcode_atts(array(
+        "page-id" => ''
+    ), $atts));
+    $args = array ( 'category' => ID, 'posts_per_page' => 5);
+    $myposts = get_posts( $args );
+    foreach( $myposts as $post ) :	setup_postdata($post);
+
+    endforeach;
+}
